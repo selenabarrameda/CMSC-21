eec556cacc3bc353de3e5f74125eed254d62e72c
selenabarrameda <98042521+selenabarrameda@users.noreply.github.com>
2022-03-22 15:15:06 +0800

    Update as1.c

--- Lecture 3/as1.c
+++ Lecture 3/as1.c
@@ -4,15 +4,13 @@ int main(void){
 	
 	int age, teenager; 
 	
-	age = 10; // (can be modified)
+	// age = 10; (can be modified)
 	
-	if (13 <= age <= 19){
-		printf("is a teen");
-		teenager = 1;
+	if (13 <= age && age <= 19){
+		teenager = true;
 	}
 	else{
-		printf("is not a teen");
-		teenager = 0;
+		teenager = false;
 	}		
 }
 
