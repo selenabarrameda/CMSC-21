0229de4baf32b1de48887c3223cace405d6f420a
selenabarrameda <98042521+selenabarrameda@users.noreply.github.com>
2022-04-22 16:13:16 +0800

    Add files via upload

---
+++ Long Exam/LE_1(test iv #8b).c
@@ -0,0 +1,34 @@
+#8.) 
+a.) if ( b == 3){
+	if (a == 2){
+		printf("*****");
+		printf(">>>>>");
+		printf("<<<<<");
+	}
+	else{
+		printf("-----");
+	}
+}
+	
+  b.) if (b == 3){
+	if (a == 2){
+		printf("*****");
+	}
+	else{ 
+		printf(">>>>>");
+		printf("<<<<<");
+		printf("-----");
+	}
+}
+
+   c.) if ( b == 3){
+	if (a == 2){
+		printf("*****");
+		printf("<<<<<");
+	}
+	else{ 
+		printf(">>>>>");
+		printf("-----");
+	}
+}
+
